c512e3cbeefb0437be6db2684b9708cf6d59e4dd
lchansky <lchansky@mail.ru>
2023-02-05 19:16:23 +0200

    Task 1 done

--- robots/models.py
+++ robots/models.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+import re
+
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class Robot(models.Model):
@@ -6,3 +10,21 @@ class Robot(models.Model):
     model = models.CharField(max_length=2, blank=False, null=False)
     version = models.CharField(max_length=2, blank=False, null=False)
     created = models.DateTimeField(blank=False, null=False)
+
+    def custom_validation(self):
+        if not re.match(r'^[A-Z0-9]{2}-[A-Z0-9]{2}$', self.serial):
+            raise ValidationError('Model and Version must be 2 and 2 chars, like 11-XX or R3-D4 or RR-RR')
+        if self.serial != f'{self.model}-{self.version}':
+            raise ValidationError('Wrong serial name! It must be like "model-version"!')
+        if self.created > datetime.now(self.created.tzinfo):
+            raise ValidationError('Field "created" must be littler than current time')
+
+    def clean(self, *args, **kwargs):
+        self.custom_validation()
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if not self.serial and isinstance(self.model, str) and isinstance(self.version, str):
+            self.serial = self.model + '-' + self.version
+        self.full_clean()
+        super().save(*args, **kwargs)
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse, HttpResponseBadRequest
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from robots.models import Robot
+
+
+class API(APIView):
+    def post(self, request):
+        post_data = request.data
+        try:
+            robot = Robot.objects.create(**post_data)
+        except ValidationError as e:
+            return HttpResponseBadRequest(content=json.dumps(e.message_dict))
+
+        robot_dict = robot.__dict__
+        robot_dict.pop('_state')
+        return Response(data=robot_dict)
+
+
